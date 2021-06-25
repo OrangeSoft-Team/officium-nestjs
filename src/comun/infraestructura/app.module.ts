9cbe3dd803d012fd334d4566fd8bc192e1d84d92
Ricardo <rsca4321@gmail.com>
2021-06-24 23:59:47 -0400

    Instala Nest.js/TypeORM
    - Se instala TypeORM de Nest.js.
    - Se inicializa el ORM a nivel de la capa de infraestructura comun del proyecto.
    
    Este ORM se utilizará para implementar los puertos repositorios de datos que se requieran en el proyecto.

--- src/comun/infraestructura/app.module.ts
+++ src/comun/infraestructura/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
