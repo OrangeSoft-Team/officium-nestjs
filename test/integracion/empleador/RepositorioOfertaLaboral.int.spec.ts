dd1ac7562f5722327a9d952ae81082cca846ce8d
Ricardo <rsca4321@gmail.com>
2021-06-29 20:38:40 -0400

    Implementa prueba unitaria 8.5 y prueba de integración
    - Se implementa la prueba unitaria para el caso de uso 8.5 Empleador: Ver detalle de oferta laboral, implementando el mock del repositorio de persistencia necesario.
    
    - Se implementa la prueba de integración del repositorio de ofertas laborales por parte del empleador.
    
    Estas pruebas se realizan para obtener un flujo asegurado del caso de uso y poder probar automaticamente los distintos metodos del repositorio de persistencia utilizando una base de datos de testing.

---
+++ test/integracion/empleador/RepositorioOfertaLaboral.int.spec.ts
@@ -0,0 +1,112 @@
+import {
+  Connection,
+  createConnection,
+  getRepository,
+  Repository,
+} from 'typeorm'
+import { EmpresaORM } from '../../../src/comun/infraestructura/persistencia/Empresa.orm'
+import { OfertaLaboralORM } from '../../../src/comun/infraestructura/persistencia/OfertaLaboral.orm'
+import { OfertaLaboralNoExiste } from '../../../src/empleador/aplicacion/excepciones/OfertaLaboralNoExiste'
+import { OfertaLaboralYaExiste } from '../../../src/empleador/aplicacion/excepciones/OfertaLaboralYaExiste'
+import { RepositorioOfertaLaboral } from '../../../src/empleador/infraestructura/adaptadores/RepositorioOfertaLaboral'
+
+const oferta_prueba = {
+  id: '7453dc15-7ff2-4c37-9455-de661a5275b1',
+  titulo: 'Desarrollador en Python',
+  fechaPublicacion: new Date('06-06-2020'),
+  fechaModificacion: new Date('06-08-2020'),
+  cargo: 'Desarrollador',
+  sueldo: 50000,
+  descripcion:
+    'Se busca desarrollador en python moderno con amplios conocimientos en los principios SOLID.',
+  duracionEstimada: 1,
+  escalaDuracion: 'mes',
+  turno: 'diurno',
+  numeroVacantes: 1,
+  estado: 'cancelado',
+  idEmpresa: '38e33e61-c75c-4190-86ac-c77124381214',
+}
+
+const empresa_pruba = {
+  uuid: '38e33e61-c75c-4190-86ac-c77124381214',
+  codigoPostal: '1060',
+  direccionCalle: 'Avenida testing',
+  correoElectronico: 'testing@testing.com',
+  nombre: 'Testing Inc.',
+}
+
+describe('Repositorio de persistencia Empleador: Ofertas Laborales', () => {
+  let conexionBD: Connection
+  let ofertaLaboralORM: Repository<OfertaLaboralORM>
+  let empresaORM: Repository<EmpresaORM>
+  let repositorioOfertaLaboral: RepositorioOfertaLaboral
+
+  beforeAll(async () => {
+    // Generamos una conexión a la base de datos de testing
+    conexionBD = await createConnection({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'officium_test',
+      entities: [OfertaLaboralORM, EmpresaORM],
+      synchronize: true,
+    })
+
+    // Creamos los repositorios del ORM
+    ofertaLaboralORM = getRepository(OfertaLaboralORM)
+    empresaORM = getRepository(EmpresaORM)
+
+    // Eliminamos datos de prueba anteriores
+    await ofertaLaboralORM.delete('7453dc15-7ff2-4c37-9455-de661a5275b1')
+    await empresaORM.delete('38e33e61-c75c-4190-86ac-c77124381214')
+
+    // Insertamos empresa de prueba
+    await empresaORM.insert(empresa_pruba)
+
+    // Instanciamos al repositorio a prubar (Subject under testing)
+    repositorioOfertaLaboral = new RepositorioOfertaLaboral(
+      ofertaLaboralORM,
+      empresaORM,
+    )
+  })
+
+  it('Debe registrar una nueva oferta laboral con los datos especificados', () => {
+    const comando = repositorioOfertaLaboral.crear(oferta_prueba)
+
+    return comando.then((res) => {
+      expect(res).toBeUndefined()
+      const query = repositorioOfertaLaboral.obtenerOferta({
+        idEmpresa: '38e33e61-c75c-4190-86ac-c77124381214',
+        idOfertaLaboral: '7453dc15-7ff2-4c37-9455-de661a5275b1',
+      })
+      return query.then((res) => {
+        expect(res).toStrictEqual(oferta_prueba)
+      })
+    })
+  })
+
+  it('Debe rechazar el registro de una oferta laboral debido a que ya existe una con dichos identificadores', () => {
+    const comando = repositorioOfertaLaboral.crear(oferta_prueba)
+
+    return comando.catch((err) => {
+      expect(err).toBeInstanceOf(OfertaLaboralYaExiste)
+    })
+  })
+
+  it('Debe retornar un error de tipo OfertaLaboralNoExiste cuando se intenta obtener una oferta inexistente', () => {
+    const query = repositorioOfertaLaboral.obtenerOferta({
+      idEmpresa: '38e33e61-c75c-4190-86ac-c77124381214',
+      idOfertaLaboral: 'noexiste-7ff2-4c37-9455-de661a5275b1',
+    })
+
+    return query.catch((err) => {
+      expect(err).toBeInstanceOf(OfertaLaboralNoExiste)
+    })
+  })
+
+  afterAll(async () => {
+    await conexionBD.close()
+  })
+})
