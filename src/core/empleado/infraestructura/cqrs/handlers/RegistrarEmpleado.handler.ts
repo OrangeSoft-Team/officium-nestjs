c13573ea09db73637a820cd0240a3fd3212f42c1
Ricardo <rsca4321@gmail.com>
2021-07-18 02:32:49 -0400

    Implementa CQRS para Registro Empleado
    - Se implementa el COMANDO y el HANDLER de CQRS para el caso de uso correspondiente.
    
    - USM Empleado 1.1
    - USM Empleado 1.2
    - USM Empleado 2.1

---
+++ src/core/empleado/infraestructura/cqrs/handlers/RegistrarEmpleado.handler.ts
@@ -0,0 +1,49 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
+import { ServicioIdentificador } from '../../../../../comun/infraestructura/adaptadores/ServicioIdentificador'
+import { RegistrarEmpleado } from '../../../aplicacion/servicios/RegistrarEmpleado'
+import { RepositorioCiudades } from '../../adaptadores/RepositorioCiudades'
+import { RepositorioDirecciones } from '../../adaptadores/RepositorioDirecciones'
+import { RepositorioEmpleados } from '../../adaptadores/RepositorioEmpleados'
+import { RepositorioEstados } from '../../adaptadores/RepositorioEstados'
+import { RepositorioPaises } from '../../adaptadores/RepositorioPaises'
+import { EmpleadoApiMapeador } from '../../mapeadores/Empleado.api.mapeador'
+import { ComandoRegistrarEmpleado } from '../comandos/RegistrarEmpleado.comando'
+
+@CommandHandler(ComandoRegistrarEmpleado)
+export class HandlerRegistrarEmpleado
+  implements ICommandHandler<ComandoRegistrarEmpleado>
+{
+  private readonly repositorioPaises: RepositorioPaises
+  private readonly repositorioEstados: RepositorioEstados
+  private readonly repositorioCiudades: RepositorioCiudades
+  private readonly repositorioDirecciones: RepositorioDirecciones
+  private readonly repositorioEmpleados: RepositorioEmpleados
+  private readonly servicioIdentificador: ServicioIdentificador
+
+  private readonly servicioRegistrarEmpleado: RegistrarEmpleado
+
+  public constructor() {
+    this.repositorioPaises = new RepositorioPaises()
+    this.repositorioEstados = new RepositorioEstados()
+    this.repositorioCiudades = new RepositorioCiudades()
+    this.repositorioDirecciones = new RepositorioDirecciones()
+    this.repositorioEmpleados = new RepositorioEmpleados()
+    this.servicioIdentificador = new ServicioIdentificador()
+
+    this.servicioRegistrarEmpleado = new RegistrarEmpleado(
+      this.repositorioPaises,
+      this.repositorioEstados,
+      this.repositorioCiudades,
+      this.repositorioDirecciones,
+      this.repositorioEmpleados,
+      this.servicioIdentificador,
+      null,
+    )
+  }
+
+  public async execute(comando: ComandoRegistrarEmpleado) {
+    return this.servicioRegistrarEmpleado.ejecutar(
+      EmpleadoApiMapeador.transformarComandoRegistrarEmpleado(comando),
+    )
+  }
+}
