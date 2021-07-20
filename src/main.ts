faf612b4a56308816d1765d846ebb0fd7b8314cf
Ricardo <rsca4321@gmail.com>
2021-07-20 00:05:13 -0400

    Instaja npm/jwt y npm/cookie-parser
    - Se instala jsonwebtoken y cookie-parser para el codigo de infraestructura del caso de uso para iniciar sesión de los distintos usuarios.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core'
+import * as cookieParser from 'cookie-parser'
 import { AppModule } from './comun/infraestructura/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.use(cookieParser())
   await app.listen(2000)
 }
 bootstrap()
