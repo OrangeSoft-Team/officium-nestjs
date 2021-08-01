67d1821409b360aac0beb95a4f996c6f77484775
Daniel Jaspe <danijaspe@gmail.com>
2021-08-01 16:59:45 -0400

    Modifica ORM de cursoEstudiante
    -Se agrega columna primaria compuesta al ORM.

--- src/mooc/estudiante/infraestructura/persistencia/CursoEstudiante.orm.ts
+++ src/mooc/estudiante/infraestructura/persistencia/CursoEstudiante.orm.ts
@@ -5,11 +5,15 @@ import { EstudianteORM } from "./Estudiante.orm";
 @Entity('cursos_empleados')
 export class CursoEstudianteORM {
 
-    @ManyToOne(() => CursoORM, (curso) => curso.cursos_empleados)
+    @ManyToOne(() => CursoORM, (curso) => curso.cursos_empleados,{
+        primary: true,
+    })
     @JoinColumn({ name: 'uuid_curso' })
     curso: CursoORM
 
-    @ManyToOne(() => EstudianteORM, (estudiante) => estudiante.cursos_empleados)
+    @ManyToOne(() => EstudianteORM, (estudiante) => estudiante.cursos_empleados,{
+        primary: true,
+    })
     @JoinColumn({ name: 'uuid_empleado' })
     empleado: EstudianteORM
 }
