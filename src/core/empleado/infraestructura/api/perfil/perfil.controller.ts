bba32aaa559e569a5b10c00801c8e01e4e26c767
Ricardo <rsca4321@gmail.com>
2021-08-03 16:29:45 -0400

    Implementa Swagger para Core/Empleado: VerPerfilEmpleado

--- src/core/empleado/infraestructura/api/perfil/perfil.controller.ts
+++ src/core/empleado/infraestructura/api/perfil/perfil.controller.ts
@@ -1,8 +1,15 @@
 import { Body, Controller, Get } from '@nestjs/common'
 import { QueryBus } from '@nestjs/cqrs'
+import {
+  ApiBasicAuth,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger'
 import { IExcepcionAplicacion } from '../../../../../comun/aplicacion/IExcepcionAplicacion'
 import { Auth } from '../../../../../comun/infraestructura/dto/Auth.dto'
 import { QueryVerPerfilEmpleadoActual } from '../../cqrs/queries/VerPerfilEmpleado.query'
+import { VerPerfilEmpleadoApiDTO } from '../../dto/VerPerfilEmpleado.api.dto'
 import { EmpleadoApiMapeador } from '../../mapeadores/Empleado.api.mapeador'
 import { ErroresHttpPerfilEmpleado } from './perfil.errores'
 
@@ -11,6 +18,16 @@ export class ControladorPerfilEmpleado {
   public constructor(private readonly queryBus: QueryBus) {}
 
   @Get()
+  @ApiTags('Core/Empleado')
+  @ApiBasicAuth()
+  @ApiOkResponse({
+    type: VerPerfilEmpleadoApiDTO,
+    description:
+      'Los datos del perfil del empleado se obtuvieron correctamente.',
+  })
+  @ApiNotFoundResponse({
+    description: 'No se ha encontrado al empleado solicitado.',
+  })
   public async obtenerPerfil(@Body() dto: Auth<any>) {
     const solicitud = await this.queryBus.execute(
       new QueryVerPerfilEmpleadoActual({ idEmpleado: dto.idUsuario }),
