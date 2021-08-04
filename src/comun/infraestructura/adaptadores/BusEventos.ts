ae541e39d7c73b2f77282789a472937e41e56f18
Ricardo <rsca4321@gmail.com>
2021-08-04 19:38:46 -0400

    Implementa AMQP RabbitMQ Bus Eventos
    - Se implementa RabbitMQ utilizando el protocolo AMQP como implementación concreta del puerto (interfaz).
    - Se agrega la variable de entorno respectiva para soportar el protocolo AMQP.
    - Se instala AMQPLib para implementar dicho bus de eventos.

--- src/comun/infraestructura/adaptadores/BusEventos.ts
+++ src/comun/infraestructura/adaptadores/BusEventos.ts
@@ -1,9 +1,29 @@
 import { IBusEventos } from '../../aplicacion/IBusEventos'
 import { IEventoDominio } from '../../dominio/IEventoDominio'
+import { connect, Connection } from 'amqplib'
 
 export class BusEventos implements IBusEventos {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  private static instancia: BusEventos
+  private amqp: Connection
+  private readonly nombreCola: string
+
+  private constructor() {
+    this.nombreCola = 'Cola'
+  }
+
+  public static obtenerInstancia(): BusEventos {
+    if (!BusEventos.instancia) {
+      BusEventos.instancia = new BusEventos()
+    }
+
+    return BusEventos.instancia
+  }
+
   public async publicar(eventos: IEventoDominio[]): Promise<void> {
-    return
+    this.amqp = await connect(process.env.RUTA_AMQP)
+    const canal = await this.amqp.createChannel()
+
+    const json = JSON.stringify(eventos)
+    await canal.sendToQueue(this.nombreCola, Buffer.from(json))
   }
 }
